6fb343103fe6eae76fcec4da22335d6da4e7fb84
Grigory Graborenko <thegrisha@gmail.com>
2022-10-16 10:09:50 +1000

    example: shaders are now embedded

--- Examples/EmptyQT+SDLUnityBuild/UserSettings.cmake
+++ Examples/EmptyQT+SDLUnityBuild/UserSettings.cmake
@@ -6,4 +6,4 @@ set(NESHNY_DIR "C:/Code/Neshny")
 set(QT_DIR "C:/Qt/5.15.2/msvc2019_64")
 set(SDL2_DIR "C:/SDL/SDL2-2.0.14")
 set(SDL2_DIR_PATH "C:/SDL/SDL2_mixer-2.0.4")
-
+set(SHADER_PATH "src/shaders")
