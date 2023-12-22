e13f9df046290fc23160ecc1503d7fe5c42ce711
Grigory Graborenko <thegrisha@gmail.com>
2023-12-22 18:31:17 +1000

    grid cache template

--- Geometry.h
+++ Geometry.h
@@ -85,4 +85,64 @@ bool RaySphere(V sphere_pos, double sphere_rad, V ray_origin, V ray_end, V& hit_
 	return true;
 }
 
+template<typename T>
+class Grid2DCPUCache {
+
+public:
+
+	Grid2DCPUCache		( Vec2 min_pos, Vec2 max_pos, double cell_size, std::function<Vec2(const T& item)> get_position ) :
+		p_MinPos		( min_pos )
+		,p_InvCellSize	( 1.0 / cell_size )
+		,p_GridRange	( ((max_pos - min_pos) * p_InvCellSize).Ceil() )
+		,p_GetPosFunc	( get_position )
+	{
+		p_Cells.resize(p_GridRange.x * p_GridRange.y);
+	}
+
+	void	AddItem			( T& item ) {
+		GetCell(p_GetPosFunc(item)).push_back(&item);
+	}
+
+	void	AddItems		( std::vector<T>& items ) {
+		for (auto& item : items) {
+			AddItem(item);
+		}
+	}
+
+	void	Reset			( void ) { for(auto& cell: p_Cells) { cell.clear(); } }
+
+	void	Iterate			( Vec2 from_pos, Vec2 to_pos, std::function<void(T* item)> item_callback ) {
+		auto min_g = GetGridPos(from_pos);
+		auto max_g = GetGridPos(to_pos);
+		for (int x = min_g.x; x <= max_g.x; x++) {
+			for (int y = min_g.y; y <= max_g.y; y++) {
+				int index = y * p_GridRange.x + x;
+				auto& cell = p_Cells[index];
+				for (auto& item : cell) {
+					item_callback(item);
+				}
+			}
+		}
+	}
+
+private:
+
+	iVec2	GetGridPos		( Vec2 pos ) {
+		return iVec2::Max(iVec2(0, 0), iVec2::Min(p_GridRange + iVec2(-1, -1), iVec2(((pos - p_MinPos) * p_InvCellSize).Floor())));
+	}
+
+	std::vector<T*>&	GetCell	( Vec2 pos ) {
+		auto gpos = GetGridPos(pos);
+		int index = gpos.y * p_GridRange.x + gpos.x;
+		return p_Cells[index];
+	}
+
+	Vec2							p_MinPos;
+	double							p_CellSize;
+	double							p_InvCellSize;
+	iVec2							p_GridRange;
+	std::function<Vec2(const T&)>	p_GetPosFunc;
+	std::vector<std::vector<T*>>	p_Cells;
+};
+
 } // namespace Neshny
